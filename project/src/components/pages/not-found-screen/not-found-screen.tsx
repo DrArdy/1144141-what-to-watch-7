01e481e2b474184f3e9816c3143fa6bb98ac4709
DrArdy <dimakharitonov2011@yandex.ru>
2022-08-26 20:27:10 +0300

    Added routes

--- project/src/components/pages/not-found-screen/not-found-screen.tsx
+++ project/src/components/pages/not-found-screen/not-found-screen.tsx
@@ -1,18 +1,21 @@
 import Footer from '../../footer/footer';
 import Header from '../../header/header';
+import { Link } from 'react-router-dom';
 
 function NotFoundScreen(): JSX.Element {
   return (
     <div className="not-found-page">
 
-      <Header isAuthorized={ false } />
-
-      <div className="not-found-page__content">
-        <p>404 - page not found</p>
-        <a href="main.html" className="logo__link logo__link--light">Return to main page</a>
+      <div className="page-content">
+        <Header isAuthorized={ false } />
+        <div className="not-found-page__content" style={{ marginBottom: '40px', minHeight: 'calc(100vh - 336px)' }}>
+          <p style={{ textAlign: 'center' }}>404 - page not found</p>
+          <Link className="" to='/' style={{ textAlign: 'center', display: 'block', color: '#c9b37e' }}>
+            Return to main page
+          </Link>
+        </div>
+        <Footer />
       </div>
-
-      <Footer />
     </div>
   );
 }
